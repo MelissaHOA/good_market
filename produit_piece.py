800f96539c3e3704a22a0c3cc7d11e7b30b2096b
Mélissa Hoarau <melissa.hoarau@fms-ea.com>
2024-07-24 14:55:59 +0200

    Fichier produit_piece

---
+++ produit_piece.py
@@ -0,0 +1,37 @@
+import dataclasses
+from produit import *
+from commun import *
+
+
+@dataclasses.dataclass
+class Produit_Piece(Produit):
+    stock: int
+    """ Stock disponible en nombre de pièces"""
+
+
+    @abstractmethod
+    def prix_quantite(self, qte: int) -> Euro:
+        if qte is not int:
+            raise ValueError("La quantité saisi doit être un nombre entier")
+        return Euro(self.prix_unite_c.centimes() * qte)
+
+
+    @abstractmethod
+    def prendre_quantite(self, qte: int) -> QuantiteRes:
+        if qte is not int:
+            raise ValueError("La quantité saisi doit être un nombre entier")
+        if qte > self.stock:
+            return QuantiteRes.PAS_ASSEZ
+        self.stock -= qte
+        return QuantiteRes.OK
+
+
+    @abstractmethod
+    def demander_quantite(self) -> int:
+        qte_saisi = input(f"Saisir la quantité de {self.nom} : ").strip()
+        return int(qte_saisi)
+
+
+
+
+
