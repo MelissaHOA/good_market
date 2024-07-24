0b768af9d161cb476764d49f86b82029b551b4de
Mélissa Hoarau <melissa.hoarau@fms-ea.com>
2024-07-24 12:02:13 +0200

    Commentaire meta  données Début

---
+++ good_market.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Le programme a pour but de permettre aux clients de faire leurs achats
+# en choisissant les fruits et légumes souhaités et leur quantité.
+
